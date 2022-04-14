2077c8d3d05184a9aae2de4d1cf4df18a9f4997b
Riya Joshi <riyajoshi312@gmail.com>
2022-04-14 17:48:13 +0530

    Upto rendering Forms and applying Bootstrap

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\Type\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/new", name="add_task")
+     */
+    public function new(Request $request): Response
+    {
+        // creates a task object and initializes some data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form,
+        ]);
+
+        // ...
+    }
+}
+
+?>
